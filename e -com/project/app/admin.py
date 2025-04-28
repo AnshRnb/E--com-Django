15603ed3886179c6c18f4a220e7ac540160c1031
Ansh <anshrock100b@gmail.com>
2025-04-28 10:47:32 +0530

    changes in admin.py

--- e -com/project/app/admin.py
+++ e -com/project/app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+
+from .models import Student
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ['stu_name', 'stu_email', 'stu_password' ,'stu_password']
